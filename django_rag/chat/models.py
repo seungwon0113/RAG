da3b87f810e5168d863efe214f08b6bbe9f5cba9
seungwon0113 <jungseungwon0113@gmail.com>
2025-03-30 17:52:55 +0900

    ✨ VSC postgreSQL 확장자 설치 및 chat/migrations/0001_init__.py pgvector  활성화
    chat/migrations/0001_init__.py 도커나 리눅스 환경에서는 pgvector 확장자가 설치는 되었지만 활성화가 되어있지않아서 VectorExtension()함수를 호출하여 활성화 해준다

--- django_rag/chat/models.py
+++ django_rag/chat/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from pgvector.django import VectorField, HnswIndex
 
-# Create your models here.
+class Item(models.Model):
+    # 벡터 필드 정의 : dimensions=3 > 3차원 벡터
+    embedding = VectorField(dimensions=3, editable=False)
+
+    class Meta:
+        # index 설정
+        indexes = [
+            # https://github.com/pgvector/pgvector?tab=readme-ov-file#index-options
+            HnswIndex(
+                name='item_embedding_hnsw_idx',  # 유일한 이름이어야 합니다.
+                fields=['embedding'],
+                # 각 벡터를 연결할 최대 연결수
+                # 높을수록 인덱스 크기가 커지며 더 긴 구축시간, 더 정확한 결과
+                m=16,  # default: 16
+                # 인덱스 구축시 고려할 후보 개수
+                ef_construction=64,  # default: 64
+                # 인덱스 생성에 사용할 벡터 연산 클래스
+                opclasses=['vector_cosine_ops']
+            ),
+        ]
