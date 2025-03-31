8daa30d4748c366e818a11c919c0e09545a54b1e
seungwon0113 <jungseungwon0113@gmail.com>
2025-03-31 13:48:49 +0900

    ✨ Added post
    게시글 앱추가

--- django_rag/mysite/urls.py
+++ django_rag/mysite/urls.py
@@ -1,13 +1,11 @@
 from django.apps import apps
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("chat/", include("chat.urls")),
-    path("", RedirectView.as_view(url="/chat/")),
 ]
 
 if apps.is_installed("debug_toolbar"):
